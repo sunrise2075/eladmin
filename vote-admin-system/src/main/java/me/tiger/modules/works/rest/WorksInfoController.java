70f0943f70b948dda2efd89ce9e67219055c1c4e
sunrise2075 <sunrise2075@163.com>
2021-07-24 22:30:14 +0800

    修复多文件上传文件解析失败问题

--- vote-admin-system/src/main/java/me/tiger/modules/works/rest/WorksInfoController.java
+++ vote-admin-system/src/main/java/me/tiger/modules/works/rest/WorksInfoController.java
@@ -27,8 +27,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -46,7 +44,7 @@ import java.util.stream.Collectors;
  * @website https://el-admin.vip
  * @date 2021-07-24
  **/
-@Controller
+@RestController
 @RequiredArgsConstructor
 @Api(tags = "作品信息管理")
 @RequestMapping("/api/worksInfo")
