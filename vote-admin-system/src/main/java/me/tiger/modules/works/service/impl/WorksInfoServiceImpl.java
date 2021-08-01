af6d54dd737763c86a57c00d3f6f1e25f69251d2
sunrise2075 <sunrise2075@163.com>
2021-08-01 18:33:28 +0800

    后台作品列表使用微信用户昵称

--- vote-admin-system/src/main/java/me/tiger/modules/works/service/impl/WorksInfoServiceImpl.java
+++ vote-admin-system/src/main/java/me/tiger/modules/works/service/impl/WorksInfoServiceImpl.java
@@ -69,9 +69,30 @@ public class WorksInfoServiceImpl implements WorksInfoService {
 
     @Override
     public Map<String, Object> queryAll(WorksInfoQueryCriteria criteria, Pageable pageable) {
+
         WorksInfo worksInfo = WorksInfo.builder().authorName(criteria.getAuthorName()).authorMobile(criteria.getAuthorMobile()).type(criteria.getType()).build();
         Page<WorksInfo> page = worksInfoRepository.findAll(Example.of(worksInfo), pageable);
-        return PageUtil.toPage(page.map(worksInfoMapper::toDto));
+
+        List<String> wxIds = page.getContent().stream().map(WorksInfo::getWxOpenId).collect(Collectors.toList());
+        if (!CollectionUtils.isEmpty(wxIds)) {
+
+            Map<String, WxWorksAuthor> wxWorksAuthorMap = wxWorksAuthorRepository.findAuthors(wxIds).stream().collect(Collectors.toMap(WxWorksAuthor::getOpenId, o -> o));
+
+            page.getContent().forEach(w -> {
+                if (wxWorksAuthorMap.containsKey(w.getWxOpenId())) {
+                    w.setAuthorName(wxWorksAuthorMap.get(w.getWxOpenId()).getNickName());
+                }
+            });
+        }
+
+        //修改后台界面显示的用户名位微信昵称
+        Page<WorksInfoDto> worksInfoDtoPage = page.map(worksInfoMapper::toDto);
+        List<WorksInfoDto> content = worksInfoDtoPage.getContent();
+
+
+
+
+        return PageUtil.toPage(worksInfoDtoPage);
     }
 
     @Override
